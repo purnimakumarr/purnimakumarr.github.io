cc824bbe29b295ae30408b2b77245daba5987599
Purnima Kumar <purnimakumar2021@gmail.com>
2022-07-01 21:57:18 +0530

    update

--- js/script.js
+++ js/script.js
@@ -47,68 +47,8 @@ window.addEventListener("scroll", function () {
 });
 
 //////////////////////////////////////////
-// MAKE DESCRIPTION BUTTONS WORK
-const descShort = document.querySelector(".desc--short");
-const btnShort = document.querySelector(".btn--short");
-
-const descMed = document.querySelector(".desc--med");
-const btnMed = document.querySelector(".btn--med");
-
-const descLong = document.querySelector(".desc--long");
-const btnLong = document.querySelector(".btn--long");
-
-btnShort.addEventListener("click", function () {
-  btnShort.classList.add("btn-selected");
-  btnMed.classList.remove("btn-selected");
-  btnLong.classList.remove("btn-selected");
-
-  descMed.classList.remove("desc-show");
-  descLong.classList.remove("desc-show");
-});
-
-btnMed.addEventListener("click", function () {
-  btnMed.classList.add("btn-selected");
-  btnShort.classList.remove("btn-selected");
-  btnLong.classList.remove("btn-selected");
-
-  descMed.classList.add("desc-show");
-  descLong.classList.remove("desc-show");
-});
-
-btnLong.addEventListener("click", function () {
-  btnLong.classList.add("btn-selected");
-  btnShort.classList.remove("btn-selected");
-  btnMed.classList.remove("btn-selected");
-
-  descMed.classList.add("desc-show");
-  descLong.classList.add("desc-show");
-});
-
-//////////////////////////////////////////
 // MAKING MOBILE NAVIAGTION WORK
 const btnEl = document.querySelector(".btn-mobile-nav");
 btnEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
-
-//////////////////////////////////////////
-// FIXING FLEXBOX GPA PROPERTY MISSING IN SAFARI VERSIONS
-const noFlexGap = function () {
-  const flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
-
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
-
-  document.body.appendChild(flex);
-
-  var isSupported = flex.scrollHeight === 1;
-  flex.parentNode.removeChild(flex);
-  console.log(isSupported);
-
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
-};
-
-noFlexGap();
